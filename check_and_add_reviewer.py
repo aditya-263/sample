e291515be259dd4d4072af77fb8d7bc226d6ffb0
adityapatidar263 <adityapatidar263@example.com>
2024-08-27 13:30:01 +0530

    ASDdsadassa

--- check_and_add_reviewer.py
+++ check_and_add_reviewer.py
@@ -12,7 +12,6 @@ STRING_TO_CHECK = 'zzzzz'
 
 print("hellllooooooo")
 print(PR_NUMBER)
-print(GITHUB_TOKEN)
 # API headers
 headers = {
     'Authorization': f'token {GITHUB_TOKEN}',
@@ -33,29 +32,29 @@ for file in files:
 
 print("hellllooooooo2")
 # Check if the specific file is among the changed files
-# file_changed = any(file['filename'] == FILE_TO_CHECK for file in files)
+file_changed = any(file['filename'] == FILE_TO_CHECK for file in files)
 
 
-# if file_changed:
-#     # Get the content of the file
-#     file_url = f"{GITHUB_API_URL}/repos/{REPO}/contents/{FILE_TO_CHECK}?ref=refs/heads/main"
-#     response = requests.get(file_url, headers=headers)
-#     file_content = response.json().get('content', '')
-#     file_content = requests.utils.unquote(file_content)  # Decode base64 content
+if file_changed:
+    # Get the content of the file
+    file_url = f"{GITHUB_API_URL}/repos/{REPO}/contents/{FILE_TO_CHECK}?ref=refs/heads/main"
+    response = requests.get(file_url, headers=headers)
+    file_content = response.json().get('content', '')
+    file_content = requests.utils.unquote(file_content)  # Decode base64 content
 
-#     if STRING_TO_CHECK in file_content:
-#         # Add the reviewer to the pull request
-#         reviewers_url = f"{GITHUB_API_URL}/repos/{REPO}/pulls/{PR_NUMBER}/requested_reviewers"
-#         payload = {
-#             'reviewers': [REVIEWER_USERNAME]
-#         }
-#         response = requests.post(reviewers_url, headers=headers, json=payload)
+    if STRING_TO_CHECK in file_content:
+        # Add the reviewer to the pull request
+        reviewers_url = f"{GITHUB_API_URL}/repos/{REPO}/pulls/{PR_NUMBER}/requested_reviewers"
+        payload = {
+            'reviewers': [REVIEWER_USERNAME]
+        }
+        response = requests.post(reviewers_url, headers=headers, json=payload)
 
-#         if response.status_code == 201:
-#             print(f"Successfully added reviewer {REVIEWER_USERNAME} to PR {PR_NUMBER}.")
-#         else:
-#             print(f"Failed to add reviewer. Status code: {response.status_code}")
-#     else:
-#         print(f"String '{STRING_TO_CHECK}' not found in {FILE_TO_CHECK}.")
-# else:
-#     print(f"{FILE_TO_CHECK} was not changed in the PR.")
+        if response.status_code == 201:
+            print(f"Successfully added reviewer {REVIEWER_USERNAME} to PR {PR_NUMBER}.")
+        else:
+            print(f"Failed to add reviewer. Status code: {response.status_code}")
+    else:
+        print(f"String '{STRING_TO_CHECK}' not found in {FILE_TO_CHECK}.")
+else:
+    print(f"{FILE_TO_CHECK} was not changed in the PR.")
